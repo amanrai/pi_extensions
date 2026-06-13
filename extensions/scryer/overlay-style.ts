3af082d881921bb3b253bd27b674ef5a0e888231
amanrai <aman.rai25@gmail.com>
2026-06-13 20:27:04 +0530

    Invert overlay palette

--- extensions/scryer/overlay-style.ts
+++ extensions/scryer/overlay-style.ts
@@ -1,11 +1,11 @@
 import { truncateToWidth, visibleWidth } from "@earendil-works/pi-tui";
 
 const RESET = "\x1b[0m";
-const BG = "\x1b[48;2;255;255;255m";
-const FG = "\x1b[38;2;0;0;0m";
-const MUTED = "\x1b[38;2;0;0;0m";
-const CYAN = "\x1b[38;2;8;145;178m";
-const AMBER = "\x1b[38;2;180;83;9m";
+const BG = "\x1b[48;2;0;0;0m";
+const FG = "\x1b[38;2;255;255;255m";
+const MUTED = "\x1b[38;2;255;255;255m";
+const CYAN = "\x1b[38;2;255;255;255m";
+const AMBER = "\x1b[38;2;255;255;255m";
 const BOLD = "\x1b[1m";
 
 function padAnsi(s: string, width: number): string {
